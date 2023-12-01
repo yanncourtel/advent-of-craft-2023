88d78dad9901a3e4d298ca8e55c5064e1676603d
Yann Courtel <courtel.yann@gmail.com>
2023-12-01 13:06:55 +0100

    simplify if else

--- exercise/day01/src/main/java/food/Food.java
+++ exercise/day01/src/main/java/food/Food.java
@@ -8,12 +8,8 @@ public record Food(LocalDate expirationDate,
                    Boolean approvedForConsumption,
                    UUID inspectorId) {
     public boolean isEdible(Supplier<LocalDate> now) {
-        if (this.expirationDate.isAfter(now.get()) &&
+        return this.expirationDate.isAfter(now.get()) &&
                 this.approvedForConsumption &&
-                this.inspectorId != null) {
-            return true;
-        } else {
-            return false;
-        }
+                this.inspectorId != null;
     }
 }
