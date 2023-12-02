6677f5cc7f2aa97976a945b5c012e7f8dcfeeb3f
Yann Courtel <courtel.yann@gmail.com>
2023-12-02 22:54:57 +0100

    organize the conditions to remove indentation

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -5,23 +5,22 @@ public class FizzBuzz {
     }
     
     public static String convert(Integer input) throws OutOfRangeException {
-        if (input > 0) {
-            if (input <= 100) {
-                if (input % 3 == 0 && input % 5 == 0) {
-                    return "FizzBuzz";
-                }
-                if (input % 3 == 0) {
-                    return "Fizz";
-                }
-                if (input % 5 == 0) {
-                    return "Buzz";
-                }
-                return input.toString();
-            } else {
-                throw new OutOfRangeException();
-            }
-        } else {
+        if (input <= 0 || input > 100) {
             throw new OutOfRangeException();
         }
+
+        if (input % 3 == 0 && input % 5 == 0) {
+            return "FizzBuzz";
+        }
+
+        if (input % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (input % 5 == 0) {
+            return "Buzz";
+        }
+
+        return input.toString();
     }
 }
