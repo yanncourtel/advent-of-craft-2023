af896912f85b0cd8393933ad0dd84e89b8929bd0
Yann Courtel <courtel.yann@gmail.com>
2023-12-02 23:10:17 +0100

    remove magic string. Last optimizations

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -4,31 +4,47 @@ public class FizzBuzz {
 
     public static final int MIN_RANGE = 0;
     public static final int MAX_RANGE = 100;
+    public static final int FIZZ_MULTIPLE = 3;
+    public static final int BUZZ_MULTIPLE = 5;
+    public static final String FIZZ_STRING = "Fizz";
+    public static final String BUZZ_STRING = "Buzz";
 
     private FizzBuzz() {
     }
-    
+
     public static String convert(Integer input) throws OutOfRangeException {
-        if (isInputInRange(input)) {
-            throw new OutOfRangeException();
+        checkInputIsInRange(input);
+
+        if (isFizzBuzz(input)) {
+            return FIZZ_STRING + BUZZ_STRING;
         }
 
-        if (input % 3 == 0 && input % 5 == 0) {
-            return "FizzBuzz";
+        if (isFizz(input)) {
+            return FIZZ_STRING;
         }
 
-        if (input % 3 == 0) {
-            return "Fizz";
-        }
-
-        if (input % 5 == 0) {
-            return "Buzz";
+        if (isBuzz(input)) {
+            return BUZZ_STRING;
         }
 
         return input.toString();
     }
 
-    private static boolean isInputInRange(Integer input) {
-        return input <= MIN_RANGE || input > MAX_RANGE;
+    private static void checkInputIsInRange(Integer input) throws OutOfRangeException {
+        if (input <= MIN_RANGE || input > MAX_RANGE) {
+            throw new OutOfRangeException();
+        }
+    }
+
+    private static boolean isFizzBuzz(Integer input) {
+        return isFizz(input) && isBuzz(input);
+    }
+
+    private static boolean isFizz(Integer input) {
+        return input % FIZZ_MULTIPLE == 0;
+    }
+
+    private static boolean isBuzz(Integer input) {
+        return input % BUZZ_MULTIPLE == 0;
     }
 }
