a69caa399768f0275bf5625588a50af5bb5218f0
Yann Courtel <courtel.yann@gmail.com>
2023-12-03 16:47:03 +0100

    improve assertions of the person owning the youngest pet with mutable list

--- exercise/day03/src/test/java/PopulationTests.java
+++ exercise/day03/src/test/java/PopulationTests.java
@@ -4,16 +4,10 @@ import people.Person;
 import people.Pet;
 import people.PetType;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
-import static java.lang.Integer.MAX_VALUE;
-import static java.lang.String.format;
-import static java.lang.System.lineSeparator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PopulationTests {
@@ -21,37 +15,48 @@ class PopulationTests {
 
     @BeforeAll
     static void setup() {
-        population = Arrays.asList(
-                new Person("Peter", "Griffin")
-                        .addPet(PetType.CAT, "Tabby", 2),
-                new Person("Stewie", "Griffin")
-                        .addPet(PetType.CAT, "Dolly", 3)
-                        .addPet(PetType.DOG, "Brian", 9),
-                new Person("Joe", "Swanson")
-                        .addPet(PetType.DOG, "Spike", 4),
-                new Person("Lois", "Griffin")
-                        .addPet(PetType.SNAKE, "Serpy", 1),
-                new Person("Meg", "Griffin")
-                        .addPet(PetType.BIRD, "Tweety", 1),
-                new Person("Chris", "Griffin")
-                        .addPet(PetType.TURTLE, "Speedy", 4),
-                new Person("Cleveland", "Brown")
-                        .addPet(PetType.HAMSTER, "Fuzzy", 1)
-                        .addPet(PetType.HAMSTER, "Wuzzy", 2),
-                new Person("Glenn", "Quagmire")
-        );
+        population = new ArrayList<>();
+
+        population.add(new Person("Peter", "Griffin")
+                .addPet(PetType.CAT, "Tabby", 2));
+
+        population.add(new Person("Stewie", "Griffin")
+                .addPet(PetType.CAT, "Dolly", 3)
+                .addPet(PetType.DOG, "Brian", 9));
+
+        population.add(new Person("Joe", "Swanson")
+                .addPet(PetType.DOG, "Spike", 4));
+
+        population.add(new Person("Lois", "Griffin")
+                .addPet(PetType.SNAKE, "Serpy", 1));
+
+        population.add(new Person("Meg", "Griffin")
+                .addPet(PetType.BIRD, "Tweety", 1));
+
+        population.add(new Person("Chris", "Griffin")
+                .addPet(PetType.TURTLE, "Speedy", 4));
+
+        population.add(new Person("Cleveland", "Brown")
+                .addPet(PetType.HAMSTER, "Fuzzy", 1)
+                .addPet(PetType.HAMSTER, "Wuzzy", 2));
+
+        population.add(new Person("Glenn", "Quagmire"));
     }
 
     @Test
     void whoOwnsTheYoungestPet() {
-        var filtered = population.stream()
+        var personWithAPetThatWasJustBorn =
+                new Person("Glenn", "Quagmire")
+                        .addPet(PetType.HAMSTER, "Toto", 0);
+
+        population.add(personWithAPetThatWasJustBorn);
+
+        var personWithYoungestPet = population.stream()
                 .min(Comparator
                     .comparingInt(PopulationTests::getYoungestPetAgeForPerson))
                 .orElse(null);
 
-        assert filtered != null;
-        assertThat(filtered.firstName())
-                .isEqualTo("Lois");
+        assertThat(personWithYoungestPet).isEqualTo(personWithAPetThatWasJustBorn);
     }
 
     private static int getYoungestPetAgeForPerson(Person person) {
