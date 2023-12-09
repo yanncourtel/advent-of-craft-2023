dde06da96a2f8f75be908446ca443367d37a5a83
Yann Courtel <courtel.yann@gmail.com>
2023-12-09 20:18:56 +0100

    refactor: add building method for password validation in tests

--- exercise/day08/src/test/java/PasswordValidationTests.java
+++ exercise/day08/src/test/java/PasswordValidationTests.java
@@ -9,31 +9,30 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PasswordValidationTests {
-
-    public static Stream<Arguments> invalidPasswords() {
-        return Stream.of(
-            Arguments.of("Pass", "Contains at least 8 characters"),
-            Arguments.of("passwordddd", "Contains at least one capital letter"),
-            Arguments.of("PPPPPPPPPPPP", "Contains at least one lowercase letter"),
-            Arguments.of("Passworddd", "Contains at least a number"),
-            Arguments.of("Pass12rddd", "Contains at least a special character in this list `. * # @ $ % &`"),
-            Arguments.of("P@ss12rddd!", "Any other characters are not authorized."),
-            Arguments.of("P@ss12rddd````", "Any other characters are not authorized.")
-        );
-    }
-
     @Test
     void passwordValid() {
-        var validationPassword = new ValidationPassword();
-
-        assertTrue(validationPassword.validate("P@ssw0rd"));
+        assertTrue(createPasswordValidation().validate("P@ssw0rd"));
     }
 
     @ParameterizedTest
     @MethodSource("invalidPasswords")
     void passwordIsInvalid(String invalidPassword) {
-        var validationPassword = new ValidationPassword();
+        assertFalse(createPasswordValidation().validate(invalidPassword));
+    }
 
-        assertFalse(validationPassword.validate(invalidPassword));
+    public static Stream<Arguments> invalidPasswords() {
+        return Stream.of(
+                Arguments.of("Pass", "Contains at least 8 characters"),
+                Arguments.of("passwordddd", "Contains at least one capital letter"),
+                Arguments.of("PPPPPPPPPPPP", "Contains at least one lowercase letter"),
+                Arguments.of("Passworddd", "Contains at least a number"),
+                Arguments.of("Pass12rddd", "Contains at least a special character in this list `. * # @ $ % &`"),
+                Arguments.of("P@ss12rddd!", "Any other characters are not authorized."),
+                Arguments.of("P@ss12rddd````", "Any other characters are not authorized.")
+        );
+    }
+
+    private static ValidationPassword createPasswordValidation() {
+        return new ValidationPassword();
     }
 }
