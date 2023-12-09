596504aea0fd286e8d0964155e5b1357d3371460
Yann Courtel <courtel.yann@gmail.com>
2023-12-09 18:24:23 +0100

    refactor: add improved version of method for any other character not allowed

--- exercise/day08/src/main/java/password/ValidationPassword.java
+++ exercise/day08/src/main/java/password/ValidationPassword.java
@@ -1,11 +1,8 @@
 package password;
 
 import java.util.List;
-import java.util.function.IntPredicate;
 
 public class ValidationPassword {
-    public static final int MINIMUM_PASSWORD_SIZE = 8;
-    private final String allowedSpecialCharacters = ".*#@$%&";
 
     private final List<PasswordRule> rules = List.of(
             new MinimumSizeRule(),
@@ -13,48 +10,15 @@ public class ValidationPassword {
             new ContainsAtLeastOneLowerCaseRule(),
             new ContainsAtLeastANumberRule(),
             new ContainsAtLeastOneAllowedSpecialCharacterRule()
-
     );
 
     public boolean validate(String passwordString) {
         return rules.stream().allMatch(r -> r.validate(passwordString))
-                //&& containsUpperCase(passwordString)
-                //&& containsLowerCase(passwordString)
-                //&& containsNumber(passwordString)
-                //&& containsAllowedSpecialCharacter(passwordString)
-                && doesNotContainAnyForbiddenCharacter(passwordString);
+                && doesNotContainAnyForbiddenCharacterRegex(passwordString);
     }
 
-    private static boolean hasMinimumSize(String passwordString) {
-        return passwordString.length() >= MINIMUM_PASSWORD_SIZE;
-    }
-
-    private boolean containsUpperCase(String value) {
-        return contains(value, i -> Character.isLetter(i) && Character.isUpperCase(i));
-    }
-
-    private boolean containsLowerCase(String value) {
-        return contains(value, i -> Character.isLetter(i) && Character.isLowerCase(i));
-    }
-
-    private boolean containsNumber(String value) {
-        return contains(value, Character::isDigit);
-    }
-
-    private boolean containsAllowedSpecialCharacter(String value) {
-        return contains(value, i -> allowedSpecialCharacters.indexOf(i) != -1);
-    }
-
-    private boolean doesNotContainAnyForbiddenCharacter(String passwordString) {
-        return !contains(passwordString,
-                i -> !Character.isLetter(i)
-                        && !Character.isLowerCase(i)
-                        && !Character.isUpperCase(i)
-                        && allowedSpecialCharacters.indexOf(i) == -1
-                        && !Character.isDigit(i));
-    }
-
-    private boolean contains(String value, IntPredicate predicate) {
-        return value.chars().anyMatch(predicate);
+    private boolean doesNotContainAnyForbiddenCharacterRegex(String passwordString) {
+        String pattern = "^[a-zA-Z0-9.*#@$%&]+$";
+        return passwordString.matches(pattern);
     }
 }
