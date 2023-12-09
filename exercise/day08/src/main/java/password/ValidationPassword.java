f28351bc220d799c4d27ef810baf4de6d5e11cfe
Yann Courtel <courtel.yann@gmail.com>
2023-12-09 17:15:08 +0100

    green: pass test for password invalid with at least one lowercase charater

--- exercise/day08/src/main/java/password/ValidationPassword.java
+++ exercise/day08/src/main/java/password/ValidationPassword.java
@@ -5,7 +5,12 @@ import java.util.function.IntPredicate;
 public class ValidationPassword {
     public boolean validate(String passwordString) {
         return passwordString.length() >= 8
-                && containsUpperCase(passwordString);
+                && containsUpperCase(passwordString)
+                && containsLowerCase(passwordString);
+    }
+
+    private boolean containsLowerCase(String value) {
+        return contains(value, i -> Character.isLetter(i) && Character.isLowerCase(i));
     }
 
     private boolean containsUpperCase(String value) {
