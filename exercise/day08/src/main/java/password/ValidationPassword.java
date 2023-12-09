918186aede1807c37717fd928ce36375796f9563
Yann Courtel <courtel.yann@gmail.com>
2023-12-09 17:07:42 +0100

    green: pass test for password invalid with at least one upper charater

--- exercise/day08/src/main/java/password/ValidationPassword.java
+++ exercise/day08/src/main/java/password/ValidationPassword.java
@@ -1,7 +1,18 @@
 package password;
 
+import java.util.function.IntPredicate;
+
 public class ValidationPassword {
     public boolean validate(String passwordString) {
-        return passwordString.length() >= 8;
+        return passwordString.length() >= 8
+                && containsUpperCase(passwordString);
+    }
+
+    private boolean containsUpperCase(String value) {
+        return contains(value, i -> Character.isLetter(i) && Character.isUpperCase(i));
+    }
+
+    private boolean contains(String value, IntPredicate predicate) {
+        return value.chars().anyMatch(predicate);
     }
 }
