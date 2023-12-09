1b0c9340d06daa31dcdbbe641a7a9f118a04818f
Yann Courtel <courtel.yann@gmail.com>
2023-12-09 17:00:22 +0100

    green: pass test for password invalid with less than 8 characters

--- exercise/day08/src/main/java/password/ValidationPassword.java
+++ exercise/day08/src/main/java/password/ValidationPassword.java
@@ -2,6 +2,8 @@ package password;
 
 public class ValidationPassword {
     public boolean validate(String passwordString) {
+        if (passwordString.length() < 8)
+            return false;
         return true;
     }
 }
