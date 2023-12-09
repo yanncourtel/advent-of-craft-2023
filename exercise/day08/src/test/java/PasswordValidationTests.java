3b37a77f7130ba595d246fcd1b46251e7be01f70
Yann Courtel <courtel.yann@gmail.com>
2023-12-09 17:12:21 +0100

    refactor: parameterize test for invalid passwords

--- exercise/day08/src/test/java/PasswordValidationTests.java
+++ exercise/day08/src/test/java/PasswordValidationTests.java
@@ -1,36 +1,42 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import password.ValidationPassword;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PasswordValidationTests {
     /*
-- Contains at least 8 characters
-- Contains at least one capital letter
+-- Contains at least 8 characters
+-- Contains at least one capital letter
 - Contains at least one lowercase letter
 - Contains at least a number
 - Contains at least a special character in this list `. * # @ $ % &`.
 - Any other characters are not authorized.
     */
 
+    public static Stream<Arguments> invalidPassword() {
+        return Stream.of(
+                Arguments.of("P@ssw", "Contains at least 8 characters"),
+                Arguments.of("passwordddd", "Contains at least one capital letter")
+        );
+    }
+
     @Test
     void passwordValid() {
         var validationPassword = new ValidationPassword();
 
-        assertTrue(validationPassword.validate("P@ssw0rd!"));
+        assertTrue(validationPassword.validate("P@ssw0rd"));
     }
 
-    @Test
-    void passwordIsInvalidWithLessThan8Characters() {
+    @ParameterizedTest
+    @MethodSource("invalidPassword")
+    void passwordIsInvalid(String invalidPassword) {
         var validationPassword = new ValidationPassword();
 
-        assertFalse(validationPassword.validate("P@ssw"));
-    }
-
-    @Test
-    void passwordIsInvalidIfNotAtLeastOneUpperCharacter() {
-        var validationPassword = new ValidationPassword();
-
-        assertFalse(validationPassword.validate("passwordddd"));
+        assertFalse(validationPassword.validate(invalidPassword));
     }
 }
