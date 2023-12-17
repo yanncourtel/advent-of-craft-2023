f4b034e1df4684e6153311b3e98f4e4ecb7bfcf5
Yann Courtel <courtel.yann@gmail.com>
2023-12-17 21:07:27 +0100

    refactor: optimize tests and code

--- exercise/day09/src/main/java/account/Client.java
+++ exercise/day09/src/main/java/account/Client.java
@@ -6,22 +6,9 @@ import java.util.stream.Collectors;
 
 public class Client {
     private final Map<String, Double> orderLines;
-    private double totalAmount;
 
     public Client(Map<String, Double> orderLines) {
         this.orderLines = orderLines;
-    }
-
-    public String toStatement() {
-        return orderLines.entrySet().stream()
-                .map(entry -> formatLine(entry.getKey(), entry.getValue()))
-                .collect(Collectors.joining(System.lineSeparator()))
-                .concat(System.lineSeparator() + "Total : " + totalAmount + "€");
-    }
-
-    private String formatLine(String name, Double value) {
-        totalAmount += value;
-        return name + " for " + value + "€";
     }
 
     public double getTotalAmount() {
@@ -29,6 +16,22 @@ public class Client {
                 .stream()
                 .mapToDouble(x -> x)
                 .sum();
+    }
+
+    public String toStatement() {
+        return orderLines.entrySet()
+                .stream()
+                .map(entry -> formatLine(entry.getKey(), entry.getValue()))
+                .collect(Collectors.joining(System.lineSeparator()))
+                .concat(formatTotal());
+    }
+
+    private String formatLine(String name, Double value) {
+        return name + " for " + value + "€";
+    }
+
+    private String formatTotal() {
+        return System.lineSeparator() + "Total : " + getTotalAmount() + "€";
     }
 }
 
