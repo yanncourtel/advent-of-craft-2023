98fbea6661116ce987907e943b0fc279e2756fe8
Yann Courtel <courtel.yann@gmail.com>
2023-12-23 20:55:21 +0100

    add functional map

--- exercise/day10/src/main/java/games/FizzBuzz.java
+++ exercise/day10/src/main/java/games/FizzBuzz.java
@@ -1,5 +1,10 @@
 package games;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class FizzBuzz {
     public static final int MIN = 0;
     public static final int MAX = 100;
@@ -7,27 +12,26 @@ public class FizzBuzz {
     public static final int BUZZ = 5;
     public static final int FIZZBUZZ = 15;
 
+    public static Map<Predicate<Integer>, Function<Integer, String>> fizzBuzzMap = new LinkedHashMap<>();
+
+    static {
+        fizzBuzzMap.put(i -> is(FIZZBUZZ, i), i -> "FizzBuzz");
+        fizzBuzzMap.put(i -> is(FIZZ, i), i -> "Fizz");
+        fizzBuzzMap.put(i -> is(BUZZ, i), i -> "Buzz");
+        fizzBuzzMap.put(i -> true, Object::toString);
+    }
+
     private FizzBuzz() {
     }
 
     public static String convert(Integer input) throws OutOfRangeException {
-        if (isOutOfRange(input)) {
-            throw new OutOfRangeException();
-        }
-        return convertSafely(input);
-    }
-
-    private static String convertSafely(Integer input) {
-        if (is(FIZZBUZZ, input)) {
-            return "FizzBuzz";
-        }
-        if (is(FIZZ, input)) {
-            return "Fizz";
-        }
-        if (is(BUZZ, input)) {
-            return "Buzz";
-        }
-        return input.toString();
+        return fizzBuzzMap.entrySet()
+                .stream()
+                .filter(f -> !isOutOfRange(input))
+                .filter(f -> f.getKey().test(input))
+                .findFirst()
+                .map(f -> f.getValue().apply(input))
+                .orElseThrow(OutOfRangeException::new);
     }
 
     private static boolean is(Integer divisor, Integer input) {
